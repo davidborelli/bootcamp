835f7d8600cc906858ab71ad8c0905285ad8e08b
David <david_borelli@live.com>
2019-08-26 19:38:58 -0300

    Service provider listing

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddlewate from './app/middlewares/auth';
 
@@ -19,6 +20,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewate);
 
 routes.post('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 // file is the name of the field that will be sent in the request
 
