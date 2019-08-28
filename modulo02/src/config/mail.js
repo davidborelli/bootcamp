93cff6f0cf5ff0553983b8c22c659c6ae94b734f
David <david_borelli@live.com>
2019-08-27 22:32:44 -0300

    Configuring Nodemailer

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,23 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '195dc76647a266',
+    pass: '5463a490524e50',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Serviços de e-mail
+ * Amazon SES
+ * Sparkpost
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * Gmail (bad)
+ * Mailtrap (Dev)
+ */
