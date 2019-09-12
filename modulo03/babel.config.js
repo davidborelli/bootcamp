c6e5c8b1e757f00ac47e76914fa10da3dead2d9e
David <david_borelli@live.com>
2019-09-12 19:50:22 -0300

    Initial config to run ReactJS project

---
+++ modulo03/babel.config.js
@@ -0,0 +1,6 @@
+module.export = {
+  presets: [
+    "@babel/preset-env", //Make things understood that browser does not understand, example: import / export
+    "@babel/preset-react" //Make things understood that browser does not understand, example: JSX
+  ]
+};
