cbea67da765a596b517a29fae9dc2744e878a01d
David <david_borelli@live.com>
2019-10-08 19:52:56 -0300

    Global Styles

--- modulo06/src/App.js
+++ modulo06/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+/* BrowserRouter está no app para o Header ter acesso as rotas */
 function App() {
-  return <h1>Hellow World</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
