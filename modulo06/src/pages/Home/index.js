ad6da1a085ffd6bc2458084ea09e22e49e382de3
David <david_borelli@live.com>
2019-10-11 22:20:19 -0300

    Displaying quantities

--- modulo06/src/pages/Home/index.js
+++ modulo06/src/pages/Home/index.js
@@ -39,6 +39,8 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
+
     return (
       <S.ProductList>
         {products.map(product => (
@@ -52,7 +54,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -63,11 +66,19 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Home);
 // Connect retorna outra função, por esse motivo os dois parentes, pois
