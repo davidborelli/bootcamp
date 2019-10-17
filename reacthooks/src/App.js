a6534b658dfe042bc9d8d70ff4387206b3e12e06
David <david_borelli@live.com>
2019-10-17 20:48:58 -0300

    Hook useState

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [techs, setTech] = useState(['React Native', 'ReactJS', 'Node.js']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = () => {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  };
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={event => setNewTech(event.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Inserir
+      </button>
+    </>
+  );
 }
 
 export default App;
