e25a29e610486f1612325d328956ed505f8cc001
David <david_borelli@live.com>
2019-10-17 21:21:50 -0300

    Hook useEffect()

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTech] = useState(['React Native', 'ReactJS', 'Node.js']);
+  const [techs, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   const handleAdd = () => {
     setTech([...techs, newTech]);
     setNewTech('');
   };
+
+  // simulando didMount | Caracteristica é que não gerencia ninguém, ou seja executa somente quando montar
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // willUnmount, vai ser executada sempre que o componente deixar de existir
+    // Pode ser retornado para cada um dos useEffect()
+    return () => {};
+  }, []);
+
+  /* Recebe uma função que vai ser executada, segundo parametro é quando vai ser
+   executada(arr de variaveis que vai ficar ouvindo)
+   Simulando didUpdate
+   */
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
