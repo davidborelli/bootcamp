87381bf37022b69d8247b301d54fd5dfcd00e346
David <david_borelli@live.com>
2019-10-26 13:27:58 -0300

    Validations

--- modulo07/src/pages/SignUp/index.js
+++ modulo07/src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatótio'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatório'),
+});
 
 export default function SignUp() {
   const handleSubmit = data => {
@@ -13,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="Logo GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
