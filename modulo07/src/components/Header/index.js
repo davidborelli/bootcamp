0fd2f505d930621b228f7ae7e2ebf05cf7891b0a
David <david_borelli@live.com>
2019-10-30 18:38:47 -0300

    Configuring Header

---
+++ modulo07/src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-purple.svg';
+
+import * as S from './styles';
+
+export default function Header() {
+  return (
+    <>
+      <S.Container>
+        <S.Content>
+          <nav>
+            <img src={logo} alt="Logo GoBarber" />
+            <Link to="/dashboard">Dashboard</Link>
+          </nav>
+
+          <aside>
+            <S.Profile>
+              <div>
+                <strong>David Borelli</strong>
+                <Link to="/profile">Meu perfil</Link>
+              </div>
+              <img
+                src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                alt="Usuário avatar"
+              />
+            </S.Profile>
+          </aside>
+        </S.Content>
+      </S.Container>
+    </>
+  );
+}
--- modulo07/src/pages/Dashboard/index.js
+++ modulo07/src/pages/Dashboard/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import api from '~/services/api';
 
 export default function DashBoard() {
   return <h1>DashBoard</h1>;
