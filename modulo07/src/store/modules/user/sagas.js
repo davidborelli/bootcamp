6126d6e075f2b3bd13b9bd4338006a9396f909ad
David <david_borelli@live.com>
2019-11-06 22:08:49 -0300

    Profile picture update

--- modulo07/src/store/modules/user/sagas.js
+++ modulo07/src/store/modules/user/sagas.js
@@ -7,16 +7,15 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
     const profile = {
       // Unir 2 objetos
       name,
       email,
+      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
-
-    console.tron.log(profile);
 
     const response = yield call(api.put, 'users', profile);
 
