7902a515137cd092e78fd510efd8fa5f0a83decd
David <david_borelli@live.com>
2019-11-07 09:17:29 -0300

    Dashboard Stylization

--- modulo07/src/pages/Dashboard/index.js
+++ modulo07/src/pages/Dashboard/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
 
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
+
+import * as S from './styles';
+
 export default function DashBoard() {
-  return <h1>DashBoard</h1>;
+  return (
+    <S.Container>
+      <header>
+        <button type="button">
+          <MdNavigateBefore size={36} color="#FFF" />
+        </button>
+        <strong>30 de maio</strong>
+        <button type="button">
+          <MdNavigateNext size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <S.Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </S.Time>
+        <S.Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+        <S.Time>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+        <S.Time>
+          <strong>11:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+      </ul>
+    </S.Container>
+  );
 }
