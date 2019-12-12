5c7abfa2934bb0e46050fc0f38c7508d02497da2
David <david_borelli@live.com>
2019-12-12 00:51:01 -0300

    Configuring Redux

--- modulo08GoBarberApp/gobarber/src/config/ReactotronConfig.js
+++ modulo08GoBarberApp/gobarber/src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure() //Se tiver no android passar o host aqui dentro {}
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure() //Se tiver no android passar o host aqui dentro {}
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
