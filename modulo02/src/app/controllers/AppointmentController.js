e84acb4f6a934a339bd82809abc8c8793749f110
David <david_borelli@live.com>
2019-12-16 14:15:26 -0300

    Correct Api

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -3,6 +3,7 @@ import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 import Notification from '../schemas/Notification';
 
 import CancellationMail from '../jobs/CancellationsMail';
@@ -12,29 +13,28 @@ class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
 
+    console.log('passou');
+
     const appointment = await Appointment.findAll({
-      where: {
-        user_id: req.userId,
-        canceled_at: null,
-        order: ['date'],
-        attributes: ['id', 'date', 'past', 'cancelable'],
-        limit: 20,
-        offset: (page - 1) * 20,
-        include: [
-          {
-            model: 'User',
-            as: 'provider',
-            attributes: ['id', 'name'],
-            include: [
-              {
-                model: 'File',
-                as: 'avatar',
-                attributes: ['id', 'path', 'url'],
-              },
-            ],
-          },
-        ],
-      },
+      where: { user_id: req.userId ? req.userId : null, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
     });
 
     return res.json(appointment);
