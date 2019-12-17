4e6d6285e2c37ce63fe2e52c1a75c569ec6a7b58
David <david_borelli@live.com>
2019-12-17 10:26:00 -0300

    List of Providers

--- modulo08GoBarberApp/gobarber/src/pages/New/SelectProvider/index.js
+++ modulo08GoBarberApp/gobarber/src/pages/New/SelectProvider/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 
 import * as S from './styles';
 
-export default function SelectProvider() {
-  return <Background></Background>;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    const loadProviders = async () => {
+      const response = await api.get('providers');
+      setProviders(response.data);
+    };
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <S.Container>
+        <S.ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <S.Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <S.Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <S.Name>{provider.name}</S.Name>
+            </S.Provider>
+          )}
+        />
+      </S.Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
