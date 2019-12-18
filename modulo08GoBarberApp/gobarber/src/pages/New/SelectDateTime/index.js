b01c8904354f7db8799938ef499afcbd4ef4189c
David <david_borelli@live.com>
2019-12-18 13:58:35 -0300

    Selecting time

--- modulo08GoBarberApp/gobarber/src/pages/New/SelectDateTime/index.js
+++ modulo08GoBarberApp/gobarber/src/pages/New/SelectDateTime/index.js
@@ -1,21 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { View } from 'react-native';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
 import * as S from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    const loadAvailable = async () => {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    };
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  const handleSelectHour = time => {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  };
 
   return (
     <Background>
       <S.Container>
         <DateInput date={date} onChange={setDate} />
+
+        <S.HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <S.Hour
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}
+            >
+              <S.Title>{item.time}</S.Title>
+            </S.Hour>
+          )}
+        />
       </S.Container>
     </Background>
   );
